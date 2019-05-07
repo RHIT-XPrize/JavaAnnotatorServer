db305f6d8efeaeefb5b231841e27e945cf1e75ad
Kniermj <37094855+Kniermj@users.noreply.github.com>
2019-05-07 17:05:37 -0400

    started base for http server

--- NLPAnnotator/src/main/java/annotatorServer/Annotator.java
+++ NLPAnnotator/src/main/java/annotatorServer/Annotator.java
@@ -1,9 +1,15 @@
 package annotatorServer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Annotator {
 	
+	List<AnnotatorType> annotatorTypes;
+	List<String> nameTypes;
 	public Annotator() {
-		// TODO Auto-generated constructor stub
+		nameTypes = new ArrayList<>();
+		annotatorTypes = new ArrayList<>();
 	}
 
 }
--- NLPAnnotator/src/main/java/annotatorServer/AnnotatorType.java
+++ NLPAnnotator/src/main/java/annotatorServer/AnnotatorType.java
@@ -1,5 +1,18 @@
 package annotatorServer;
 
-public abstract class AnnotatorType {
+import java.util.List;
 
+public abstract class AnnotatorType{
+
+	private String name;
+	public AnnotatorType(String name) {
+		this.name = name;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public abstract List<String> getFields();
+	
 }
