c26c9b8626302667c857e6c6c8eafc78761c8bf6
Tyler J. Bath <bathtj@rose-hulman.edu>
2019-05-12 17:37:22 -0400

    metadata compiler test case written and returns correct JSON

---
+++ NLPAnnotator/src/main/java/MetadataCompiler/MetadataAnnotationType.java
@@ -0,0 +1,30 @@
+package MetadataCompiler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
+
+import annotatorServer.AnnotationType;
+
+public class MetadataAnnotationType extends AnnotationType {
+	
+	MetaBlock chosenBlock;
+
+	public MetadataAnnotationType(String name, MetaBlock finalBlock) {
+		super(name);
+		
+		this.chosenBlock = finalBlock;
+	}
+
+	@Override
+	public List<String> getFields() {
+		List<String> output = new ArrayList<>();
+		Gson gson = new Gson();
+		
+		output.add(gson.toJson(this.chosenBlock));
+		
+		return output;
+	}
+
+}
--- NLPAnnotator/src/main/java/MetadataCompiler/MetadataAnnotator.java
+++ NLPAnnotator/src/main/java/MetadataCompiler/MetadataAnnotator.java
@@ -1,8 +1,11 @@
 package MetadataCompiler;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import SpatialRelationGenerator.InnerBlock;
 import annotatorServer.Annotator;
 
 public class MetadataAnnotator extends Annotator{
@@ -15,7 +18,6 @@ public class MetadataAnnotator extends Annotator{
 		super();
 		relationKeywords = new ArrayList<>();
 		degrees = new ArrayList<>();
-		// TODO Auto-generated constructor stub
 	}
 	
 
@@ -28,18 +30,48 @@ public class MetadataAnnotator extends Annotator{
 		MetaBlock output = compiler.chooseBlock(relationKeywords, degrees, startBlock);
 		
 		//Convert to JSON
-		return null;
+		MetadataAnnotationType annotation= new MetadataAnnotationType("\"edu.rosehulman.aixprize.pipeline.types.MetadataBlock\"", output);
+		
+		return "{" + annotation.getName() + ": "+ annotation.getFields() + "}";
 	}
 	
 	public void parseJson(String request){
 		//Loop to create all blocks in a Map of id's to MetaBlocks
+		Map<Integer, MetaBlock> blocksFromJson = new HashMap<>();
+		
+			//--------------------- test input ---------------------
+			MetaBlock origin = new MetaBlock(0,0,0,0, "origin");
+			blocksFromJson.put(0,origin);
+					
+			MetaBlock front = new MetaBlock(1,0,0,10, "front");
+			blocksFromJson.put(1,front);
+			
+			MetaBlock leftFront = new MetaBlock(2,-7,0,11, "leftFront");
+			blocksFromJson.put(2,leftFront);
+			//--------------------- test input ---------------------
 		
 		//Then Loop through JSON again, populate spatial relation lists of each block
 		
+			//--------------------- test input ---------------------
+			blocksFromJson.get(0).front.add(blocksFromJson.get(1));
+			blocksFromJson.get(1).left.add(blocksFromJson.get(2));
+			//--------------------- test input ---------------------
 		
 		//Find the starting block
+			
+			//--------------------- test input ---------------------
+			this.startBlock = blocksFromJson.get(0);
+			//--------------------- test input ---------------------
 		
 		//Find the relation keywords and degree
+			
+			//--------------------- test input ---------------------
+			this.degrees.add(1);
+			this.degrees.add(1);
+			
+			this.relationKeywords.add("FRONT");
+			this.relationKeywords.add("LEFT");
+			//--------------------- test input ---------------------
 	}
 
 }
