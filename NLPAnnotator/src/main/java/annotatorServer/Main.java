95c4c77ad2c234f5e9951992752d398e4ea509b9
schmidmj <mitchellsch@live.com>
2020-02-04 15:45:29 -0500

    rename test main

--- NLPAnnotator/src/main/java/annotatorServer/Main.java
+++ NLPAnnotator/src/main/java/annotatorServer/Main.java
@@ -12,7 +12,7 @@ import SpatialRelationGenerator.SpatialRelationAnnotator;
 import TextToSpeech.TextToSpeechAnnotator;
 import helloWorld.JavaHelloWorldAnnotator;
 
-public class TestMain {
+public class Main {
 
 	public static void main(String[] args) {
 
